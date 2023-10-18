dddd4aa7bb0f27c75ac5909cc686581ae7fe37a5
vickyroy3381 <128716321+vickyroy3381@users.noreply.github.com>
2023-10-18 21:27:39 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -9,9 +9,9 @@ const GoogleStrategy = require ('passport-google-oauth2').Strategy;
 passport.use (
   new GoogleStrategy (
     {
-      clientID: '969938984278-dr89d6qu6bv6ofc6l31qnq8mff4ou240.apps.googleusercontent.com',
-      clientSecret: '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',
-      callbackURL: 'http://localhost:5000/auth/google/callback',//http://localhost:5000/auth/google/callback
+      clientID: '7721274803-0ne13d4l0ftj6jadbplvamq1p53cn2uh.apps.googleusercontent.com',
+      clientSecret: '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',
+      callbackURL: 'https://nodejsauth-navjot.onrender.com/auth/google/callback',//http://localhost:5000/auth/google/callback
       passReqToCallback: true,
     },
     function (request, accessToken, refreshToken, profile, done) {
